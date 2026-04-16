a0ce2e52952a2887be2718983c331946ce2f03e7
Raihan Zaky Ramadhan <zakyramadhan114@gmail.com>
2026-04-16 23:17:34 +0700

    prak3

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\KategoriController;
 
 /* Home */
 Route::get('/', [HomeController::class, 'index']);
@@ -22,3 +24,6 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
 /* Sales */
 Route::get('/sales', [SalesController::class, 'index']);
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
